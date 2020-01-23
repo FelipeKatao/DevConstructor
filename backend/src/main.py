d1c197b4d3859d9d753e0906b03bb3e2d3a2aea9
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-01-23 02:45:17 -0300

    Update main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,2 +1,14 @@
 #Codigo principal para executar as rotas no FLASK
+from flask import Flask
+from flask_restful import Resource, Api
 
+
+app = Flask(__name__)
+api = Api(app)
+
+class developerBase(Resource):
+    def get(self):
+        return {"nome":"Felipe"}
+api.add_resource(developerBase,'/dev')
+if __name__ == '__main__':
+    app.run(debug=True)
