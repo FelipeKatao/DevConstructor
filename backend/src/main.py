6fb511e7e8244295c817b367441b424837d80b00
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-01-24 20:08:30 -0300

    API Rest with Flask
    Back end add with Api Rest with flask, where DELTE,ADD and GET users from static list from DataBase

--- backend/src/main.py
+++ backend/src/main.py
@@ -8,20 +8,59 @@ app = Flask(__name__)
 api = Api(app)
 
 #variavel para armazenar os devs
-USERS_DEV=[]
+USERS_DEV=[
+      {
+    "github": "MarcoHub",
+    "id": 1,
+    "techs": "JavaScript",
+    "username": "Marcos"
+  },
+  {
+    "github": "FelipeKatao",
+    "id": 0,
+    "techs": "Python",
+    "username": "Felipe"
+  },
+  {
+    "github": "LeYorn",
+    "id": 0,
+    "techs": "C#",
+    "username": "Leonardo"
+  }
+]
 
+#Modifica desenvolvedores adiciona ou remove
 class developerModify(Resource):
     def put(self,id,name,type):
         dados = json.loads(request.data)
+        dados['id']=id
+        dados['username']=name
         USERS_DEV.append(dados)
         return jsonify(dados)
-        pass
+    def delete(self,id,name,type):
+        USERS_DEV.pop(id)
+        return jsonify({"Status":"User deleted from system with sucess"})
 
+#Retorna todos os desenvolvedores
 class developerBase(Resource):
-    def get(self):
+     def get(self):
         return jsonify(USERS_DEV)
-        
+
+#Procura os desenvolvedores pelo nome
+class searchBase(Resource):
+    def get(self):
+        SearchEngine = request.args.get('developer')
+        flag=""
+        for item in USERS_DEV:
+            if item['username'] == SearchEngine:
+                return jsonify(item)
+                break
+            else:
+                flag="Dont found developer"
+        return  flag
+
 api.add_resource(developerBase,'/dev/')
-api.add_resource(developerModify,'/dev/<int:id>;<string:name>;<string:type>/')
+api.add_resource(developerModify,'/dev/mod/<int:id>;<string:name>;<string:type>/')
+api.add_resource(searchBase,'/dev/search/')
 if __name__ == '__main__':
     app.run(debug=True)
